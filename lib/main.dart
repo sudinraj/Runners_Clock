5a0cefa1ad8aaee1dd96ff092d752f785e80603d
sudinraj <sudinrajbanshi@gmail.com>
2024-12-31 15:35:20 -0600

    Timer works
    Timer works now

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,13 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
+        
         title: 'Runners Clock',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 62, 198, 247)),
         ),
+        
         //sets the starting page
         home: MyHomePageState(),
       ),
@@ -48,15 +50,27 @@ class MyHomePageState extends StatefulWidget {
 
 //main page where you set the time
 class _MyHomePageStateState extends State<MyHomePageState> {
-  int _currentWalkValue = 2;
-  int _currentRunValue = 2;
+//initial values for the number wheel
+  int _currentWalkValue = 1;
+  int _currentRunValue = 1;
+  int _currentWalkMinValue = 2;
+  int _currentRunMinValue = 2;
 
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      body: Center(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [const Color.fromARGB(255, 202, 201, 201),Colors.black],
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -74,26 +88,109 @@ class _MyHomePageStateState extends State<MyHomePageState> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text('Walk:',
-                style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,), textAlign: TextAlign.center,),
 //The number Picking Wheel
-                  NumberPicker(
-                    value: _currentWalkValue,
-                    minValue: 1,
-                    maxValue: 60,
-                    onChanged: (value) =>setState(() =>_currentWalkValue = value),
-                  ),
+                  Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Min',
+                        style: TextStyle(fontSize: 20,), textAlign: TextAlign.center,),
+
+                        NumberPicker(
+                          textStyle: TextStyle(color: const Color.fromARGB(255, 30, 186, 233)),
+                          selectedTextStyle: TextStyle(color: const Color.fromARGB(255, 7, 85, 255), fontSize: 30),
+                          value: _currentWalkValue,
+                          minValue: 0,
+                          maxValue: 60,
+                          onChanged: (value) =>setState(() =>_currentWalkValue = value),
+                        ),
+                      ],
+                    ),
+
+                    Column(
+                      children: [
+                        SizedBox(height: 25,),
+                        Text(':',
+                        style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold,), textAlign: TextAlign.center,),
+                      ],
+                    ),
+
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Sec',
+                        style: TextStyle(fontSize: 20,), textAlign: TextAlign.center,),
+
+                        NumberPicker(
+                          textStyle: TextStyle(color: const Color.fromARGB(255, 30, 186, 233)),
+                          selectedTextStyle: TextStyle(color: const Color.fromARGB(255, 7, 85, 255), fontSize: 30),
+                          value: _currentWalkMinValue,
+                          minValue: 1,
+                          maxValue: 60,
+                          onChanged: (value) =>setState(() =>_currentWalkMinValue = value),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
                 ]),
+
                 Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text('Run:',
-                style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
-                  NumberPicker(
-                    value: _currentRunValue,
-                    minValue: 1,
-                    maxValue: 60,
-                    onChanged: (value) =>setState(() =>_currentRunValue = value),
-                  ),
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,), textAlign: TextAlign.center,),
+
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Min',
+                        style: TextStyle(fontSize: 20,), textAlign: TextAlign.center,),
+
+                        NumberPicker(
+                          textStyle: TextStyle(color: Colors.pink),
+                          selectedTextStyle: TextStyle(color: const Color.fromARGB(255, 255, 7, 7), fontSize: 30),
+                          value: _currentRunValue,
+                          minValue: 0,
+                          maxValue: 60,
+                          onChanged: (value) =>setState(() =>_currentRunValue = value),
+                        ),
+                      ],
+                    ),
+
+                    Column(
+                      children: [
+                        SizedBox(height: 25,),
+                        Text(':',
+                        style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold,), textAlign: TextAlign.center,),
+                      ],
+                    ),
+
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+
+                      children: [
+                        Text('Sec',
+                        style: TextStyle(fontSize: 20,), textAlign: TextAlign.center,),
+
+                        NumberPicker(
+                          textStyle: TextStyle(color: Colors.pink),
+                          selectedTextStyle: TextStyle(color: const Color.fromARGB(255, 255, 7, 7), fontSize: 30),
+                          value: _currentRunMinValue,
+                          minValue: 1,
+                          maxValue: 60,
+                          onChanged: (value) =>setState(() =>_currentRunMinValue = value),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
                 ])
               ],
             ),
@@ -102,7 +199,7 @@ class _MyHomePageStateState extends State<MyHomePageState> {
             ElevatedButton(
               onPressed: () {
                 appState.getNext();
-                Navigator.push(context, CupertinoPageRoute(builder: (context) => RunningPageState(_currentWalkValue, _currentRunValue)));
+                Navigator.push(context, CupertinoPageRoute(builder: (context) => RunningPageState((_currentWalkValue*60 + _currentWalkMinValue), (_currentRunValue*60 + _currentRunMinValue))));
               },
               child: Text('Start'),
             ),
@@ -126,36 +223,39 @@ class RunningPageState extends StatefulWidget {
 class _RunningPageStateState extends State<RunningPageState> {
   
   Timer? _timer;
+  String activity = 'Run';
   bool done = false;
   bool set = false;
   late int remainingTime;
-  late int SetTime;
+  late int setTime;
   late int walking;
   late int running;
 
-//TODO: Timer is not working properly; The count down is too fast, and also make it able to be restart after going to first screen
-
+//Timer Starts with the run time, then it starts a countdown for the walk timer, then it loops that until stopped.
+//TODO: Make noise when timer runs out
   void startTimer(){
-  const oneSec = Duration(seconds: 10);
-  _timer = Timer.periodic(oneSec, (Timer timer){
+  const oneSec = Duration(seconds: 1);
+  _timer = Timer.periodic(oneSec, (timer){
     if(!done){
+  //when the timer is 0, it checks if it was the running timer or the walking timer, then starts the next timer
       if (remainingTime <= 0) {
           setState(() {
             timer.cancel();
-            if(SetTime == running){
-              SetTime = walking;
+            if(setTime == running){
+              setTime = walking;
               remainingTime = walking;
+              activity = 'Walk';
             }
             else{
-              SetTime = running;
+              setTime = running;
               remainingTime = running;
+              activity = 'Run';
             }
             startTimer();
           });
         } else {
           setState(() {
             remainingTime--;
-            print(remainingTime);
           });
         }
     } else{
@@ -172,42 +272,39 @@ class _RunningPageStateState extends State<RunningPageState> {
 
   @override
   Widget build(BuildContext context) {
+    //setting the value of each variable
     walking = widget.walking;
     running = widget.running;
     if(!set){
-      SetTime = running;
+      setTime = running;
       remainingTime = running;
       set = true;
+      startTimer();
     }
-    startTimer();
+    
     var appState = context.watch<MyAppState>();
-    var pair = appState.current;
 
     return Scaffold(
-      body: Center(
+//using container for background colors and stuff
+      body: Container(
+//sets the width and height to be the size of the screen
+        width: double.maxFinite,
+        height: double.maxFinite,
+        decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [const Color.fromARGB(255, 202, 201, 201),Colors.black],
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Minutes:',
+            Text(activity,
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold,), textAlign: TextAlign.center,),
+            Text('Seconds:',
                 style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
-                //A widget that I made for displaying things
-                  BigCard(pair: pair),
-                ]),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Seconds:',
-                style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
-                  BigCard(pair: pair),
-                ])
-              ],
-            ),
+                  TimeShow(remainingTime: remainingTime),
             
             SizedBox(height: 30,),
             ElevatedButton(
@@ -226,28 +323,21 @@ class _RunningPageStateState extends State<RunningPageState> {
   }
 }
 
-class BigCard extends StatelessWidget {
-  const BigCard({
+class TimeShow extends StatelessWidget {
+  const TimeShow({
     super.key,
-    required this.pair,
+    required this.remainingTime,
   });
 
-  final WordPair pair;
+  final int remainingTime;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
+      color: theme.colorScheme.onTertiary,
     );
 
-    return Card(
-      color: theme.colorScheme.primary,
-      
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Text(pair.asLowerCase, style: style),
-      ),
-    );
+    return Text(remainingTime.toString(), style: style);
   }
 }
